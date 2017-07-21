0ade8ac2bcbe0137513b7102209e002934aaa6ed
ldevernay <boblehamster@yahoo.fr>
2017-07-21 16:49:14 +0200

    init

---
+++ app.js
@@ -0,0 +1,22 @@
+console.log("Starting app.js.");
+
+const fs = require('fs');
+const _ = require('lodash');
+
+const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log("Command: ", command);
+if (command === 'add'){
+  console.log("Adding a new note");
+} else if (command === "list"){
+console.log("Listing all notes");
+}else if (command === "reading"){
+console.log("Reading notes");
+}else if (command === "list"){
+console.log("Listing all notes");
+}else if (command === "remove"){
+console.log("Removing node");
+}else {
+  console.log("Command  ")
+}
