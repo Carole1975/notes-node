74ec411c05203957eabcac4491a0a3f1527950d7
ldevernay <boblehamster@yahoo.fr>
2017-07-24 11:57:35 +0200

    try/catch

--- notes.js
+++ notes.js
@@ -9,9 +9,17 @@ var addNote = (title, body) => {
     body
   };
 
+try {
+  var notesString = fs.readFileSync('notes-data.json');
+  notes = JSON.parse(notesString);
+} catch(e) {
+  console.log("Initiated a new JSON file.");
+}
+
   notes.push(note);
   var notesString = JSON.stringify(notes);
   fs.writeFileSync('notes-data.json', notesString);
+  console.log('Successfully added: ', title);
 }
 
 var getAll = () => {
