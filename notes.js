4cff8c1a5106d92dadea57d02e63a3d93b8732cb
ldevernay <boblehamster@yahoo.fr>
2017-07-24 12:13:39 +0200

    handle duplicate notes

--- notes.js
+++ notes.js
@@ -2,24 +2,31 @@ console.log('Starting notes.js.');
 
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch(e) {
+    console.log("No note found!");
+    return [];
+  }
+};
+
+var saveNotes = () => {
+
+};
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     title,
     body
   };
 
-  try {
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-  } catch(e) {
-    console.log("Initiated a new JSON file.");
-  }
-
   var duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length == 0) {
-  notes.push(note); 
+  notes.push(note);
   var notesString = JSON.stringify(notes);
   fs.writeFileSync('notes-data.json', notesString);
   console.log('Successfully added: ', title);
