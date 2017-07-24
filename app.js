d7b8b141b2804353290262bf0223981a6120555a
ldevernay <boblehamster@yahoo.fr>
2017-07-24 11:02:02 +0200

    functions

--- app.js
+++ app.js
@@ -2,21 +2,24 @@ console.log("Starting app.js.");
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log("Command: ", command);
+console.log("Yargs: ", argv);
+
+
 if (command === 'add'){
-  console.log("Adding a new note");
+  notes.addNote(argv.title, argv.body);
 } else if (command === "list"){
-console.log("Listing all notes");
-}else if (command === "reading"){
-console.log("Reading notes");
-}else if (command === "list"){
-console.log("Listing all notes");
+  notes.getAll();
+}else if (command === "get"){
+  notes.get(argv.title);
 }else if (command === "remove"){
-console.log("Removing node");
+  notes.remove(argv.title);
 }else {
   console.log("Command  ")
 }
