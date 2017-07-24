a385b7f8c119b44310e0dcad6d0c335eafb2bc4f
ldevernay <boblehamster@yahoo.fr>
2017-07-24 11:22:53 +0200

    JSON playground + fs

--- playground/json.js
+++ playground/json.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 // var obj = {
 //   name: "Bob"
 // };
@@ -5,8 +7,22 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 //
-var personString = '{"name": "Bob", "age": 34}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log("Your name is: ", person.name);
-console.log("Your age is: ", person.age);
+// var personString = '{"name": "Bob", "age": 34}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log("Your name is: ", person.name);
+// console.log("Your age is: ", person.age);
+
+var originalNote = {
+  title: "Titre de ma premiere note",
+  body: "Blablabla blablabla"
+}
+
+var originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+var noteString = fs.readFileSync('notes.json');
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
